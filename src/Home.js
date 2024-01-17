3ad8c1c37e2e5a41d730e586616d46f1eeb538d5
remiyamazaki03 <remiyamazaki@gmail.com>
2024-01-17 22:21:32 +0900

    reorganize components, add barebones for Voicecoach page

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import developer from "./images/Remi-developer.jpg";
 
 export default function Home() {
   return (
-    <div id="content">
+    <div>
       <div className="hero row">
         <div className="col-lg-6">
           <h3>Hi!</h3>
@@ -48,132 +48,6 @@ export default function Home() {
             </p>
             <button className="launch-button">Coming soon</button>
           </div>
-        </div>
-      </div>
-      <div className="contact row" id="contact">
-        <div className="col-md-6 d-none d-md-block">
-          <h1 className="mt-4 mb-2 mb-md-5">
-            Let's <span className="name">get in touch!</span>
-          </h1>
-          <div className="d-flex">
-            <a
-              href="https://www.instagram.com/remi.0.3"
-              className="fs-1 me-3"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fa-brands fa-instagram inst-logo"></i>
-            </a>
-            <a
-              href="https://www.youtube.com/@remi03"
-              className="fs-1 mx-3"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fa-brands fa-youtube youtube-logo"></i>
-            </a>
-            <a
-              href="https://twitter.com/03_remi"
-              className="fs-1 mx-3"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fa-brands fa-x-twitter twitter-logo"></i>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/remi-yamazaki-77580b55/"
-              className="fs-1 mx-3"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fa-brands fa-linkedin linkedin-logo"></i>
-            </a>
-            <a
-              href="https://github.com/remiyamazaki03"
-              className="fs-1 ms-3"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fa-brands fa-github github-logo"></i>
-            </a>
-          </div>
-        </div>
-        <div className="col-md-6 d-flex flex-column">
-          <h1 className="d-block d-md-none my-4">
-            <span className="name">Contact</span> me
-          </h1>
-          <div className="d-flex d-block d-md-none mb-3 justify-content-center">
-            <a
-              href="https://www.instagram.com/remi.0.3"
-              className="fs-1 me-3"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fa-brands fa-instagram inst-logo"></i>
-            </a>
-            <a
-              href="https://www.youtube.com/@remi03"
-              className="fs-1 mx-3"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fa-brands fa-youtube youtube-logo"></i>
-            </a>
-            <a
-              href="https://twitter.com/03_remi"
-              className="fs-1 mx-3"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fa-brands fa-x-twitter twitter-logo"></i>
-            </a>
-            <a
-              href="https://www.linkedin.com/in/remi-yamazaki-77580b55/"
-              className="fs-1 mx-3"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fa-brands fa-linkedin linkedin-logo"></i>
-            </a>
-            <a
-              href="https://github.com/remiyamazaki03"
-              className="fs-1 ms-3"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fa-brands fa-github github-logo"></i>
-            </a>
-          </div>
-          <form className="mt-md-5 me-md-5 pe-md-5">
-            <label for="name">name</label>
-            <input
-              type="text"
-              className="form-control"
-              id="name"
-              placeholder="name"
-            />
-            <label for="email" className="mt-3">
-              email
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              placeholder="email"
-            />
-            <label for="message" className="mt-3">
-              message
-            </label>
-            <br />
-            <textarea
-              id="message"
-              className="form-control"
-              rows="5"
-              placeholder="Sorry, my form isn't working yet. Please contact me via social media while I get this up and running!"
-            ></textarea>
-            <br />
-            <button className="submit-button mt-md-4">Submit</button>
-          </form>
         </div>
       </div>
     </div>
--- src/pages/Voicecoach.js
+++ src/pages/Voicecoach.js
@@ -1,3 +1,55 @@
 export default function Voicecoach() {
-  return <h1>Voice Coach</h1>;
+  return (
+    <div>
+      <div className="title">
+        <h1>What students are saying...</h1>
+      </div>
+      <div className="content ">
+        <div className="container">
+          <div class="row">
+            <div class="col-md-6 mb-3 mb-md-0">
+              <img
+                src="images/weather-remi.png"
+                alt="weather app"
+                class="img-fluid"
+              />
+            </div>
+            <div class="col-md-6">
+              <div class="d-flex flex-column">
+                <div>
+                  <p> I really need to split css files.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6">
+              <div class="d-flex flex-column">
+                <div>
+                  <p>
+                    {" "}
+                    And adjust the mic image on this page so that the pop guard
+                    isn't cut off but it's not too big either.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 mt-5 mt-md-0">
+              <img src="images/yoga.png" alt="yoga app" class="img-fluid" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 mb-3 mb-md-0">
+              <img src="images/yogurt.png" alt="yogurt app" class="img-fluid" />
+            </div>
+            <div class="col-md-6 d-flex flex-column">
+              <div>
+                <p> This is a paragraph about my yogurt app.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
