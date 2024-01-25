d2363cf0d0338e276a80e99ac62284f631237e50
remiyamazaki03 <remiyamazaki@gmail.com>
2024-01-25 16:01:17 +0900

    update navbar.js (bootstrap 5.3)

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,16 +9,16 @@ export default function Navbar() {
       <button
         className="navbar-toggler"
         type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarToggleExternalContent"
+        aria-controls="navbarToggleExternalContent"
         aria-expanded="false"
         aria-label="Toggle navigation"
       >
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav me-auto">
+      <div className="collapse" id="navbarToggleExternalContent">
+        <ul className="navbar-nav">
           <li className="nav-item mx-0">
             <Link className="nav-link" to="/pages/performance">
               My work
