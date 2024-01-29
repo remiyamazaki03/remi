47881bfc1418d19fc2e86a20dd7c8dc437cd6280
remiyamazaki03 <remiyamazaki@gmail.com>
2024-01-29 19:03:33 +0900

    added pricing info

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,9 +6,9 @@ export default function About() {
       <h1 className="title m-0 d-flex align-items-center">About me</h1>
       <div className="about m-0 row align-items-center">
         <div className="col-md-8 p-5">
-          <h2 className="mt-2">Hi I'm Remi!</h2>
+          <h2 className="mt-2">Hi, I'm Remi!</h2>
           <p className="mt-4 bio text-center">
-            Thank you for visiting my page! <br />
+            Thank you for visiting my page 🥳 <br />
             <br />I am a classically trained singer, but my go-to genre is
             gospel. I direct the Union Praise Team (gospel and contemporary
             Christian choir) at{" "}
@@ -26,22 +26,11 @@ export default function About() {
             people achieve that. <br />
             <br />
             I'm from Japan and have lived here most of my life, but I've also
-            lived in New York City where I performed at{" "}
-            <a href="https://54below.org/" rel="noreferrer" target="blank">
-              54 Below
-            </a>{" "}
-            (Off Broadway), with the{" "}
-            <a
-              href="https://www.youtube.com/channel/UCPoeoRFfO4n1vp2zkodf5Nw"
-              rel="noreferrer"
-              target="blank"
-            >
-              Brooklyn Tabernacle Choir
-            </a>{" "}
-            (gospel choir with 6 Grammy Awards), and at numerous jazz clubs
-            around the city. Before that, I attended Princeton University, where
-            I received training in French acting and earned a Bachelor's in
-            Comparative Literature.
+            lived in New York City where I performed at 54 Below (Off Broadway),
+            with the Brooklyn Tabernacle Choir (gospel choir with 6 Grammy
+            Awards), and at numerous jazz clubs around the city. Before that, I
+            attended Princeton University, where I received training in French
+            acting and earned a Bachelor's in Comparative Literature.
             <br />
             <br /> Aside from singing, directing and teaching, I also have
             experience in voice acting / narration and basic web development.
--- src/pages/Voicecoach.js
+++ src/pages/Voicecoach.js
@@ -5,148 +5,206 @@ import stage from "../images/stage.jpg";
 export default function Voicecoach() {
   return (
     <div>
-      <div className="title d-flex align-items-center">
-        <h1>Voice lessons with Remi might be for you if...</h1>
-      </div>
-      <div className="content">
-        <div className="container py-3">
-          <div className="row py-4 px-4">
-            <div className="col-md-4 d-flex align-items-center">
+      <h1 className="title d-flex align-items-center m-0">
+        Voice lessons with Remi might be for you if...
+      </h1>
+      <div className="p-0 m-0">
+        <div className="row py-1 px-5 content">
+          <div className="col-md-4 d-flex align-items-center">
+            <img
+              src={heart}
+              alt="heartbeat"
+              className="img-fluid p-4 d-none d-md-block"
+            />
+          </div>
+          <div className="col-md-8">
+            <div>
               <img
                 src={heart}
                 alt="heartbeat"
-                className="img-fluid p-4 d-none d-md-block"
+                className="img-fluid d-block mt-4 d-md-none w-25"
               />
-            </div>
-            <div className="col-md-8">
-              <div>
-                <img
-                  src={heart}
-                  alt="heartbeat"
-                  className="img-fluid d-block d-md-none w-25"
-                />
-                <h3 className="text-center">
-                  ... you want freedom in singing, both physically AND
-                  emotionally:
-                </h3>
-                <p className="text-center">
-                  <br />
-                  “I like singing but going to karaoke with friends was
-                  embarrassing for me because I thought of myself as bad singer
-                  with limited range. During the lessons Remi always encouraged
-                  me. One of the exercises was making a pig face. She said
-                  “making an ugly face is part of the vocal lesson.{" "}
-                  <b>Nothing to be ashamed of!</b>” That's it. I had nothing to
-                  be ashamed of.
-                  <br />
-                  <br />
-                  Remi helped me to match pitches accurately and widen my range,
-                  and told me how to change from my chest voice to falsetto
-                  naturally. After the two-months training,{" "}
-                  <b>
-                    I could finally sing the song which I never expected to
-                    sing.
-                  </b>{" "}
-                  I had never tried to sing with my falsetto because it was too
-                  weak and high, but now I learned to sing strongly with my
-                  breath control and forming voice. Now my options for karaoke
-                  have increased dramatically.”
-                  <br /> - T. K. (online, Brzail)
-                </p>
-              </div>
+              <h3 className="text-center pt-md-5">
+                ... you want freedom in singing, both physically AND
+                emotionally:
+              </h3>
+              <p className="text-center px-5 pb-4">
+                <br />“
+                <b>I thought of myself as bad singer with limited range.</b>{" "}
+                During the lessons <b>Remi always encouraged me. </b>
+                One of the exercises was making a pig face. She said “making an
+                ugly face is part of the vocal lesson.{" "}
+                <b>Nothing to be ashamed of!</b>” That's it. I had nothing to be
+                ashamed of. Remi helped me to widen my range, and to change from
+                my chest voice to falsetto naturally.{" "}
+                <b>
+                  After two months of training, I could sing songs which I never
+                  expected to sing.
+                </b>
+                <br /> - T. K. (online, Brzail) <br />
+                <br /> “The result was stunning. At first, both myself and the
+                [musical] actor were quite sure of his upper range limit, but{" "}
+                <b>
+                  after Remi's coaching, he sang notes I (thought I) knew he
+                  couldn't hit, all with 100% presence and confidence onstage.
+                </b>{" "}
+                Remi encouraged and helped many of us to step up and boosted our
+                production level, and I'm very grateful to have had the
+                opportunity to work with her.” <br />- W. R-L. (Musical
+                director, USA)
+              </p>
             </div>
           </div>
-          <div className="row py-4 px-4">
-            <div className="col-md-8">
-              <div>
-                <img
-                  src={colors}
-                  alt="expand your palette"
-                  className="img-fluid d-block d-md-none w-25 pb-3"
-                />
-                <h3 className="text-center">
-                  ... you want to branch out, like singing notes or styles
-                  you've never sung before:
-                </h3>
-                <p className="text-center">
-                  <br />
-                  “I didn't know I had this side of my voice!!! Given proper
-                  instructions [guidance],{" "}
-                  <b>
-                    it was an amazing feeling to push past the limits I thought
-                    I had.
-                  </b>
-                  ” <br />- J. L. (online, USA)
-                  <br />
-                  <br />
-                  “The result was stunning. At first, both myself and the
-                  [musical] actor were quite sure of his upper range limit, but{" "}
-                  <b>
-                    after Remi's coaching, he sang notes I (thought I) knew he
-                    couldn't hit, all with 100% presence and confidence onstage.
-                  </b>{" "}
-                  Remi encouraged and helped many of us to step up and boosted
-                  our production level, and I'm very grateful to have had the
-                  opportunity to work with her.” <br />- W. R-L. (Musical
-                  director, USA)
-                </p>
-              </div>
-            </div>
-            <div className="col-md-4 d-flex align-items-center">
+        </div>
+        <div className="row py-1 px-5  content2">
+          <div className="col-md-8">
+            <div>
               <img
                 src={colors}
                 alt="expand your palette"
-                className="img-fluid p-5 d-none d-md-block"
+                className="img-fluid d-block mt-4 d-md-none w-25 pb-3"
               />
+              <h3 className="text-center pt-md-5 px-3">
+                ... you want to branch out, and sing notes and styles you've
+                never sung before:
+              </h3>
+              <p className="text-center px-5">
+                <br />
+                “I didn't know I had this side of my voice!!! Given proper
+                instructions [guidance],{" "}
+                <b>
+                  it was an amazing feeling to push past the limits I thought I
+                  had.
+                </b>
+                ” <br />- J. L. (online, USA)
+                <br />
+                <br />
+                <b>
+                  I've been singing in a choral way for more than 20 years
+                </b>{" "}
+                and singing in a different style (such as pop, musical) was very
+                challenging for me. Remi's way of teaching was very fun and also
+                very easy to understand. Thanks to her, I was able to learn how
+                to project my voice in different styles using different parts of
+                muscles!{" "}
+                <b>Now I can use my chest voice to sing powerful songs</b> and I
+                even performed in front of an audience!! Her lessons definitely
+                gave me confidence in singing!
+                <br /> - A. T. (online, Japan)
+              </p>
             </div>
           </div>
-          <div className="row py-4 px-4">
-            <div className="col-md-4 d-flex align-items-center">
+          <div className="col-md-4 d-flex align-items-center">
+            <img
+              src={colors}
+              alt="expand your palette"
+              className="img-fluid p-5 d-none d-md-block"
+            />
+          </div>
+        </div>
+        <div className="row py-1 px-5 content">
+          <div className="col-md-4 d-flex align-items-center">
+            <img
+              src={stage}
+              alt="show off your singing"
+              className="img-fluid blurry d-none d-md-block"
+            />
+          </div>
+          <div className="col-md-8">
+            <div>
               <img
                 src={stage}
                 alt="show off your singing"
-                className="img-fluid blurry d-none d-md-block"
+                className="img-fluid blurry d-block mt-4 d-md-none"
               />
-            </div>
-            <div className="col-md-8">
-              <div>
-                <img
-                  src={stage}
-                  alt="show off your singing"
-                  className="img-fluid blurry d-block d-md-none"
-                />
-                <h3 className="text-center">
-                  ... you want to take your singing to the next level, or even
-                  go professional:
-                </h3>
-                <p className="text-center">
-                  <br />
-                  “Before these lessons, all I thought about was matching pitch,
-                  but{" "}
-                  <b>
-                    I learned how to breathe, how to do vibrato, dynamics, and
-                    other techniques to help me become better at singing - all
-                    customized to my level.{" "}
-                  </b>
-                  And when I didn't understand something, Remi explained it with
-                  a lot of care.{" "}
-                  <b>I can sing so many more songs now than before!</b>” <br />-
-                  C. N. (online, Japan)
-                  <br />
-                  <br />
-                  “I'm an actor and I love musicals, but I couldn't sing. Remi
-                  was the first person who not only told me that yes, I could
-                  sing but also helped me prepare my singing audition.{" "}
-                  <b>
-                    She believed in me when I didn't believe in myself, and
-                    after that, I'm not afraid of auditioning for musicals
-                    anymore!
-                  </b>
-                  ” <br />- V. H. (in person, USA)
-                </p>
-              </div>
+              <h3 className="text-center pt-md-5">
+                ... you want to take your singing to the next level, or step up
+                your professional game:
+              </h3>
+              <p className="text-center px-5 pb-5">
+                <br />
+                “Before these lessons, all I thought about was matching pitch,
+                but{" "}
+                <b>
+                  I learned how to breathe, how to do vibrato, dynamics, and
+                  other techniques to help me become better at singing - all
+                  customized to my level.{" "}
+                </b>
+                And when I didn't understand something, Remi explained it with a
+                lot of care. I can sing so many more songs now than before!”{" "}
+                <br />- C. N. (online, Japan)
+                <br />
+                <br />
+                <br />
+                “I'm an actor and I love musicals, but I couldn't sing. Remi was
+                the first person who not only told me that yes, I could sing but
+                also helped me prepare my singing audition.{" "}
+                <b>
+                  She believed in me when I didn't believe in myself, and after
+                  that, I'm not afraid of auditioning for musicals anymore!
+                </b>
+                ” <br />- V. H. (in person, USA)
+              </p>
             </div>
           </div>
+        </div>
+        <div className="row pricing">
+          <div className="col-md-9">
+            <div>
+              <h3 className="text-center pt-5 text-decoration-underline">
+                Pricing info
+              </h3>
+              <ul className="py-3 px-5 mx-5">
+                <li className="mainList">
+                  Free trial (online only, first time only)
+                  <ul className="subList">
+                    <li>30 min, Free</li>
+                    <li>
+                      If you would like to set up a trial, please sign up via
+                      the form below!
+                    </li>
+                  </ul>
+                </li>
+                <li className="mainList">Online</li>
+                <ul className="subList">
+                  <li>Y4500 per 45 min</li>
+                  <li>
+                    If you would like to pay in a different currency, please
+                    inquire. <br />
+                    (Otherwise, the rate will be your local equivalent of the
+                    above JPY amount)
+                  </li>
+                </ul>
+                <li className="mainList">
+                  In Person (Tokyo only)
+                  <ul className="subList">
+                    <li>Y6000 per 45 min (Location: Ikebukuro area)</li>
+                    <li>
+                      If you would like to inquire about other Tokyo locations,
+                      please inquire.
+                    </li>
+                  </ul>
+                </li>
+                <br />
+                <li className="mainList">
+                  Accepted payment methods
+                  <ul className="subList">
+                    <li>
+                      PayPal, PayPay, bank transfer, cash (in person only)
+                    </li>
+                    <li>For credit card payments, please inquire.</li>
+                  </ul>
+                </li>
+              </ul>
+              <p className="text-center px-5 pb-3 fw-bold">
+                If you have questions about any of the above,
+                <br className="d-xl-none" /> please contact me via the form
+                below.
+                <br />I look forward to hearing from you!
+              </p>
+            </div>
+          </div>
+          <div className="col-md-3 d-none d-md-block"></div>
         </div>
       </div>
     </div>
