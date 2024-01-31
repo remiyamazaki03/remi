03820dde76d6a042e5dbbafb2dfee234d80b10b0
remiyamazaki03 <remiyamazaki@gmail.com>
2024-01-31 15:51:56 +0900

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,21 +9,27 @@ export default function About() {
           <h2 className="mt-2 fs-3">Hi, I'm Remi!</h2>
           <p className="mt-4 bio ">
             Thank you for visiting my page 🥳 <br />
-            <br />I am a classically trained singer, but my go-to genre is
-            gospel. I direct the Union Praise Team (gospel and contemporary
-            Christian choir) at{" "}
+            <br />I am a classically trained singer, with experience in musical
+            theatre and pop (including JPop)... but my go-to genre is gospel. I
+            direct the Union Praise Team (gospel and contemporary Christian
+            choir) at{" "}
             <a
               href="https://www.tokyounion.org"
               rel="noreferrer"
               target="blank"
             >
               Tokyo Union Church
-            </a>{" "}
-            and also teach voice lessons. I was born into a teachers' family,
-            and I will always be an educator (empowerer!) at heart. I believe
-            that <b>everyone</b> can sing, and that everyone has the right to be
-            able to express themselves freely through song - and I love helping
-            people achieve that. <br />
+            </a>
+            . <br />
+            <br />I was born into a teachers' family and have been teaching for
+            more than 15 years, and will always be an educator (empowerer!) at
+            heart. I believe that <b>everyone</b> can sing, and that everyone
+            has the right to be able to express themselves freely through song -
+            and I love helping people achieve this through{" "}
+            <a href="/voicecoach" rel="noreferrer" target="blank">
+              voice lessons
+            </a>
+            . <br />
             <br />
             I'm from Japan and have lived here most of my life, but I've also
             lived in New York City where I performed at 54 Below (Off Broadway),
@@ -32,13 +38,10 @@ export default function About() {
             attended Princeton University, where I received training in French
             acting and earned a Bachelor's in Comparative Literature.
             <br />
-            <br /> Aside from singing, directing and teaching, I also have
-            experience in voice acting / narration and basic web development.
-            <br />
             <br />
             <small>
-              For a detailed performance/arts resume, please contact me via the
-              form below.
+              For a detailed performance/arts resume or for voice samples,
+              please contact me via the form below.
             </small>
           </p>
         </div>
