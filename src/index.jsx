765306df8e8b08693e186c04acf141a52348525b
remiyamazaki03 <remiyamazaki@gmail.com>
2024-03-15 01:55:28 +0900

    migrated from CRA to Vite

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
@@ -10,7 +9,7 @@ import App from "./App";
 import Footer from "./Footer";
 import "./i18n";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
@@ -20,8 +19,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
