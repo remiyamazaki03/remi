815548b765aabe07e9ec76fac7d91c6462738c79
remiyamazaki03 <remiyamazaki@gmail.com>
2025-01-06 21:01:16 +0900

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -62,10 +62,7 @@ export default function Footer() {
             </a>
           </div>
           <p className="mt-4 mb-2 mb-md-5 highlight ps-5">
-            My contact form is currently under maintenance. Please get in touch
-            with me at hello03remi@gmail.com
-            <br />
-            お問い合わせフォームは現在メンテナンス中です。上記のメールアドレスまでお願いします。
+            email: hello03remi@gmail.com
           </p>
         </div>
         <div className="col-md-6 d-flex flex-column">
@@ -115,10 +112,7 @@ export default function Footer() {
             </a>
           </div>
           <div className="d-flex d-block d-md-none mb-3 justify-content-center">
-            My contact form is currently under maintenance. Please get in touch
-            with me at hello03remi@gmail.com
-            <br />
-            お問い合わせフォームは現在メンテナンス中です。上記のメールアドレスまでお願いします。
+            email: hello03remi@gmail.com
           </div>
           <div className="me-sm-5">
             <FormfacadeEmbed
